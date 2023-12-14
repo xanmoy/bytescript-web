cbcd1f3f4778f3a8c4ded76d1f6acc6e148219dc
Tanmoy Ganguly <xanmoy@outlook.com>
2023-12-14 20:58:23 +0530

    updated

--- app/examples/page.tsx
+++ app/examples/page.tsx
@@ -3,7 +3,7 @@ import { title } from "@/components/primitives";
 export default function ExamplesPage() {
 	return (
 		<div>
-			<h1 className={title()}>Pricing</h1>
+			<h1 className={title()}>Examples</h1>
 		</div>
 	);
 }
